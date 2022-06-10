43850ce82c80caf2a02d46089ce8848accedc92b
Apoorva0123 <47804749+Apoorva0123@users.noreply.github.com>
2022-06-10 13:47:31 +0530

    fetched

--- src/components/Women.jsx
+++ src/components/Women.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./Women.css";
 
 export default function Women() {
-  return (
-    <div>Women</div>
-  )
+
+  const [products, setProducts] = useState([]);
+
+  const fetchData = () => {
+    fetch("https://myntra-clone1.herokuapp.com/products")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setProducts(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  },[]);
+
+  return <div className="container">
+    {products.map((x,i) => {
+      return(
+        <div>
+          <img src={x.imageUrl}/>
+          <b>{x.brand}</b>
+          <p>{x.title}</p>
+          <p>Rs {x.price}/-</p>
+        </div>
+      )
+    })}
+  </div>;
 }
